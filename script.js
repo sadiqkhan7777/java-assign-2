345eeeddb1becafd956fc08c5ca5332199671ac6
sadiqkhan7777 <saiqkhansaiqkhan7777@gmail.com>
2025-01-06 22:45:24 +0500

    Javascript Assignment 2

---
+++ script.js
@@ -0,0 +1,57 @@
+// Q1) Make Your Name Function
+
+// function myName () {
+//     return("My Name is Sadiq Khan");
+// };
+
+// console.log(myName());
+
+// Q2) Make Your Name Arrow Function
+
+// const myName = () => {
+//     return("Sadiq Khan" + " " + "With arrow function");
+// };
+
+// console.log(myName());
+
+// Q3)USE EVENTS ONCLICK, ON MOUSE OVER, ON MOUSE OUT BY USING JS AND HTML
+
+function handleClick() {                   // i use hanlde for for three buttons onClick, onMouseover, OnMouseout
+    alert("Button was clicked!");
+  }
+
+// Q4) Ask for Username Using Prompt and Print with document.write
+
+// const userName = prompt("Please enter your Username:")
+// document.write(`<h1>Welcome, ${userName}!</h1>`);
+
+// Q5) If and Else Conditions for Cradle Conditions of a baby
+
+// function cradleCondition(age) {
+//     if (age < 1) {
+//       console.log("Ye shakhs ek doodh peeta bacha hai");
+//     } else if (age >= 1 && age <= 2) {
+//       console.log("Ye shakhs ek chhota bacha hai jo chalna seekh raha hai.");
+//     } else if (age > 2 && age <= 5) {
+//       console.log("Ye shakhs ek preschool ka talib-e-ilm hai.");
+//     } else if (age > 5 && age <= 7){
+//       console.log("Ye shakhs ab kindergarten mein hai.");
+//     } else {
+//       console.log("Ya Shaks ab baraha larka hay .");
+//     }
+//   }
+
+// cradleCondition(0.5);
+
+
+
+// Q6)BY USING THE BODMAS RULE
+
+// let a = 15;
+// let b = 7;
+// let c = a * b + a / b;
+
+// console.log(c); 
+
+// answer is 107.14285714285714
+
